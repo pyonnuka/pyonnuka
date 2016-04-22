6a8084063aee4879cf4554fdc3779d8d3d28b3b9
masaki_nukaga <masaki.nukaga1209@gmail.com>
2016-04-22 12:57:46 +0900

    Change return description

--- lib/pyonnuka/helpers/commands.rb
+++ lib/pyonnuka/helpers/commands.rb
@@ -1,20 +1,20 @@
-# module Pyonnuka
-#   module Helpers
-#     module Commands
-#       class << self
-#         def out_help
-#           puts <<"EOS"
-# Example:
-#     pyonnuka new ~/Code/Ruby/taskapp
-#
-#       This generates a skeletal Pyonnuka installation in ~/Code/Ruby/taskapp.
-#       See the README in the newly created application to get going.
-# Pyonnuka options:
-#     -h  # Show this help message
-#     -v  # Show this pyonnuka version
-# EOS
-#         end
-#       end
-#     end
-#   end
-# end
+module Pyonnuka
+  module Helpers
+    module Commands
+      class << self
+        def out_help
+          puts <<"EOS"
+Example:
+    pyonnuka new ~/Code/Ruby/taskapp
+
+      This generates a skeletal Pyonnuka installation in ~/Code/Ruby/taskapp.
+      See the README in the newly created application to get going.
+Pyonnuka options:
+    -h  # Show this help message
+    -v  # Show this pyonnuka version
+EOS
+        end
+      end
+    end
+  end
+end
